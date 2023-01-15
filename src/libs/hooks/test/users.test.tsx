35500d421326b76f73320a88ca3fad5b6a798c23
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2023-01-16 00:02:45 +0900

    Adding test for list api client

--- src/libs/hooks/test/users.test.tsx
+++ src/libs/hooks/test/users.test.tsx
@@ -1,7 +1,7 @@
 import { describe, beforeAll, afterAll, it, expect } from "vitest";
 import { setupServer } from "msw/node";
 import { act, renderHook } from "@testing-library/react";
-import { useUser } from "../users";
+import { useUser, useUsers } from "../users";
 import { sleep } from "../../sleep";
 import { getUserHandler } from "./libs/getUserHandler";
 import { getMockUser } from "./libs/getMockUser";
@@ -31,5 +31,26 @@ describe("libs/hooks/users", () => {
     expect(result.current.isLoading).toBeFalsy();
 
     expect(result.current.data).toEqual(getMockUser({ id: 100 }));
+  });
+
+  it("fetch user list", async () => {
+    const { result } = renderHook(() => useUsers(), {
+      wrapper: Wrapper,
+    });
+
+    expect(result.current.isLoading).toBeTruthy();
+
+    await act(sleep);
+
+    expect(result.current.data?.list).toHaveLength(5);
+    expect(result.current.isLoading).toBeFalsy();
+
+    await act(result.current.fetchNext);
+
+    expect(result.current.data?.list).toHaveLength(5);
+
+    await act(sleep);
+
+    expect(result.current.data?.list).toHaveLength(10);
   });
 });
