eb16abfc754ea85d97e0e7434c0ad67d98ffa12d
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2023-01-15 23:25:40 +0900

    Implement user api client

--- src/libs/hooks/users.tsx
+++ src/libs/hooks/users.tsx
@@ -2,7 +2,8 @@ import useSWR from "swr";
 import type { User } from "../../model/User";
 
 const fetchUser = async (userId: string): Promise<User> => {
-  return Promise.resolve({} as any);
+  const response = await fetch(`https://some.api.provider/api/user/${userId}`);
+  return response.json() as Promise<User>;
 };
 type R = typeof useSWR<User>;
 export const useUser = (userId: string): ReturnType<R> => {
