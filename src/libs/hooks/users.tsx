9646f841666cc951ed6fbeee4ce34f75afb80e3b
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2023-01-15 23:55:28 +0900

    Adding user list api client without impl

--- src/libs/hooks/users.tsx
+++ src/libs/hooks/users.tsx
@@ -1,15 +1,36 @@
 import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 import type { User } from "../../model/User";
 
 const fetchUser = async (userId: string): Promise<User> => {
   const response = await fetch(`https://some.api.provider/api/user/${userId}`);
   return response.json() as Promise<User>;
 };
-type R = typeof useSWR<User>;
-export const useUser = (userId: string): ReturnType<R> => {
+export const useUser = (userId: string): ReturnType<typeof useSWR<User>> => {
   return useSWR<User>(
     ["/api/user/:userId", userId],
     async ([, userId]) => fetchUser(userId),
+    {
+      suspense: false,
+    }
+  );
+};
+
+const fetchUsers = async (
+  offset: number = 0,
+  limit: number = 5
+): Promise<User[]> => {
+  return Promise.resolve(undefined as any);
+};
+export const useUsers = (): ReturnType<typeof useSWRInfinite<User[]>> => {
+  const keyLoader: any = () => {
+    //
+  };
+  return useSWRInfinite<User[]>(
+    keyLoader,
+    async () => {
+      return await fetchUsers();
+    },
     {
       suspense: false,
     }
