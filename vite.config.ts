8a4a061bb9f1506cb41a094679514da0b87c6ee2
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2023-01-15 14:45:43 +0900

    Adding libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, UserConfigExport } from "vite";
+import react from "@vitejs/plugin-react";
 
+const originalConfig: UserConfigExport = {
+  plugins: [react()],
+};
+const vitestConfig = {
+  test: {
+    environment: "happy-dom", // or 'jsdom', 'node'
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  ...originalConfig,
+  ...vitestConfig,
+});
